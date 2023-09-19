4561134becfc91ca0bcc198542bcd3c98205968f
Sasha678747 <145434400+Sasha678747@users.noreply.github.com>
2023-09-19 23:13:32 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    
+    public partial class Form1 : Form
+    {
+        string login = "admin";
+        string pass = "123";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            if (textBox1.Text == login && textBox2.Text == pass)
+            {
+                MessageBox.Show("Вход выполнен");
+                Form2 nf = new Form2();
+                nf.Show();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин или пароль");
+            }
+        }
+    }
+}
